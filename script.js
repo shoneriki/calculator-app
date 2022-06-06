ea5802fdf8f52d4b0b24241680622dd54b6c216c
shoneriki <ikirenohs@gmail.com>
2022-06-06 12:29:57 +0900

    background changes for each theme

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+const theme1 = document.getElementById("theme-1")
 const theme2 = document.getElementById("theme-2");
+const theme3 = document.getElementById("theme-3")
+const background = document.getElementById("background");
 
-if(theme2.checked === true) {
-  alert("clicked")
-}
+
+theme1.addEventListener("click", function() {
+  if ((theme1.checked)) {
+    background.classList.remove("two");
+    background.classList.remove("three")
+    background.classList.add("one");
+  }
+})
+
+theme2.addEventListener("click", function() {
+  if ((theme2.checked)) {
+    background.classList.remove("one");
+    background.classList.remove("three")
+    background.classList.add("two");
+  }
+})
+
+theme3.addEventListener("click", function() {
+  if ((theme3.checked)) {
+    background.classList.remove("one")
+    background.classList.remove("two")
+    background.classList.add("three")
+  }
+})
