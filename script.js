399e665b9d8c20d170e91df3af277ff9d5fc4761
shoneriki <ikirenohs@gmail.com>
2022-06-08 02:37:24 +0900

    data-theme change of bg

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
 class Calculator {
-  
+
 }
 
-// const background = document.getElementById("background");
-// const theme1 = document.getElementById("theme-1");
-// const theme2 = document.getElementById("theme-2");
-// const theme3 = document.getElementById("theme-3");
+const background = document.getElementById("background");
+const theme1 = document.getElementById("toggle-1");
+const theme2 = document.getElementById("toggle-2");
+const theme3 = document.getElementById("toggle-3");
 
-// const display = document.getElementById("display");
+const display = document.getElementById("display");
 
-// const keys = document.querySelectorAll(".keys");
+const keys = document.querySelectorAll(".keys");
 
-// theme1.addEventListener("click", function() {
-//   if ((theme1.checked)) {
-//     background.classList.remove("two");
-//     background.classList.remove("three")
-//     console.log(background)
-//     background.classList.add("one");
-//   }
-// })
+theme1.addEventListener("click", function() {
+  if ((theme1.checked)) {
+    background.setAttribute("data-theme", "one");
+  }
+})
 
-// theme2.addEventListener("click", function() {
-//   if ((theme2.checked)) {
-//     background.classList.remove("one");
-//     background.classList.remove("three");
-//     console.log(background)
-//     background.classList.add("two");
+theme2.addEventListener("click", function() {
+  if ((theme2.checked)) {
+    background.setAttribute("data-theme", "two");
+  }
+})
 
-//     display.classList.remove("one");
-//     display.classList.remove("three");
-//     console.log(display)
-//     display.classList.add("display-2");
+theme3.addEventListener("click", function() {
+  if ((theme3.checked)) {
+    background.setAttribute("data-theme", "three");
+  }
+})
 
-//   }
-// })
+function value() {
+  keys.forEach(key =>{
+    key.addEventListener("click", function() {
+      alert("clicked");
+    })
+  })
+}
 
-// theme3.addEventListener("click", function() {
-//   if ((theme3.checked)) {
-//     background.classList.remove("one")
-//     background.classList.remove("two")
-//     background.classList.add("three")
-//   }
-// })
-
-// function value() {
-//   keys.forEach(key =>{
-//     key.addEventListener("click", function() {
-//       alert("clicked");
-//     })
-//   })
-// }
-
-// value();
+value();
