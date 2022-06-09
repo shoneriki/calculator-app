9ed31b87ba8a29fbc4c85b9c5188bbc2476898f1
shoneriki <ikirenohs@gmail.com>
2022-06-09 21:32:25 +0900

    display set to 0again

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const theme1 = document.getElementById("toggle-1");
 const theme2 = document.getElementById("toggle-2");
 const theme3 = document.getElementById("toggle-3");
 
+const resetBtn = document.getElementById("resetBtn");
+const equalsBtn = document.getElementById("equalsBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+
+display.textContent =  0;
+
+
 theme1.addEventListener("click", function() {
   background.setAttribute("data-theme", "one")
 })
@@ -12,11 +19,3 @@ theme2.addEventListener("click", function () {
 theme3.addEventListener("click", function () {
   background.setAttribute("data-theme", "three");
 });
-
-const resetBtn = document.getElementById("resetBtn");
-const equalsBtn = document.getElementById("equalsBtn");
-const deleteBtn =  document.getElementById('deleteBtn');
-const numberBtns = document.querySelectorAll(".digits")
-const operatorBtns = document.querySelectorAll(".operator")
-
-const buffer = ["0"]
