eeab031b8112bebde5177c6b6a9f8ffba12480a9
shoneriki <ikirenohs@gmail.com>
2022-06-10 00:05:54 +0900

    resetBtn new way working

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const resetBtn = document.getElementById("resetBtn");
 const equalsBtn = document.getElementById("equalsBtn");
 const deleteBtn = document.getElementById("deleteBtn");
 
-const nums = document.querySelectorAll(".digits")
+
 
 theme1.addEventListener("click", function() {
   background.setAttribute("data-theme", "one")
@@ -19,14 +19,65 @@ theme3.addEventListener("click", function () {
   background.setAttribute("data-theme", "three");
 });
 
-display.textContent = 0;
+const numBtns = document.querySelectorAll(".digits");
 
-nums.forEach(num => {
-  num.addEventListener("click", () => {
-    if(display.textContent == 0) {
-      display.textContent =  "";
+const addBtn = document.getElementById("addBtn")
+
+// global
+
+//numbers to be  stored in memory
+let numArray = ['',''];
+//operator + - * /
+let operator = '';
+//index to be used for numArray
+let index = 0;
+
+display.innerText = '399,981';
+
+function reset() {
+  numArray = ['',''];
+  operator = '';
+  index = 0;
+  updateDisplay();
+}
+
+
+function updateDisplay() {
+  if(numArray[index] === '') {
+    display.innerText = '0';
+  } else {
+    display.innerText = numArray[index];
+  }
+}
+
+resetBtn.addEventListener('click', reset);
+numBtns.forEach(numBtn => {
+  numBtn.addEventListener("click", () => {
+    const regex = /^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$/;
+
+    if (regex(numArray[index] + numBtn.innerText)) {
+      numArray[i] += numBtn.innerText;
+      updateDisplay();
     }
-    let value =  parseInt(num.textContent);
-    display.textContent += value;
   })
 })
+
+
+// numBtns.forEach(numBtn => {
+//   numBtn.addEventListener("click", () => {
+//     if(display.textContent == 0) {
+//       display.textContent =  "";
+//     }
+//     let value =  parseInt(num.textContent);
+//     display.textContent += value;
+//   })
+// })
+// resetBtn.addEventListener("click", () => {
+//   display.textContent = 0;
+// })
+// deleteBtn.addEventListener("click", () => {
+
+//   let value =  display.textContent.slice(0,-1);
+//   if (value === "") {value = 0};
+//   display.textContent = value;
+// })
