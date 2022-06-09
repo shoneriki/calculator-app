c83e87905bae0c7836d66d25eaea3ec6dd5d1f91
shoneriki <ikirenohs@gmail.com>
2022-06-09 20:13:01 +0900

    JS reset

--- script.js
+++ script.js
@@ -3,45 +3,6 @@ const theme1 = document.getElementById("toggle-1");
 const theme2 = document.getElementById("toggle-2");
 const theme3 = document.getElementById("toggle-3");
 
-const display = document.getElementById("display");
-
-const keys = document.querySelectorAll(".keys");
-
-// document.body.addEventListener("click", (e) => {
-//   let target = e.target;
-//   if (e.target.id) {
-//     key = document.querySelector(`#${e.target.id}`).textContent;
-//   }
-
-//   switch (target.id) {
-//     case "9":
-//     case "8":
-//     case "7":
-//     case "6":
-//     case "5":
-//     case "4":
-//     case "3":
-//     case "2":
-//     case "1":
-//     case "0":
-//       if (value[0] == "0" && value.length === 1) {
-//         value.pop();
-//         value.push(key);
-//       } else if (isNaN(last) === false) {
-//         number = value.pop();
-//         if (number = "0") {
-//           value.push(key);
-//         } else {
-//           value.push(num + key);
-//         }
-//       } else {
-//         value.push(key);
-//       }
-//       break;
-//     case "dot"
-//   }
-// });
-
 theme1.addEventListener("click", function() {
   background.setAttribute("data-theme", "one")
 })
@@ -52,23 +13,10 @@ theme3.addEventListener("click", function () {
   background.setAttribute("data-theme", "three");
 });
 
-const calc = {
-  displayValue: '0',
-  firstInput: null,
-  waitForInput: false,
-  operator: null,
-}
+const resetBtn = document.getElementById("resetBtn");
+const equalsBtn = document.getElementById("equalsBtn");
+const deleteBtn =  document.getElementById('deleteBtn');
+const numberBtns = document.querySelectorAll(".digits")
+const operatorBtns = document.querySelectorAll(".operator")
 
-function updateDisplay(){
-  const display =  document.querySelector(".display");
-  display.textContent = calc.displayValue;
-}
-
-updateDisplay();
-
-
-keys.forEach((key) => {
-  key.addEventListener("click", function() {
-    console.log(key.textContent);
-  })
-})
+const buffer = ["0"]
